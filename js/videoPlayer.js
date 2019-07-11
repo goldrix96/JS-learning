b28ee4a723695fdd82a7a2e2f3cc88f2e4ce1454
goldrix96 <goldrix96@gmail.com>
2019-07-11 17:53:15 -0400

    dwf

--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -8,7 +8,7 @@ var videoPlayerMarkup = `
   <div class="progress">
    <div class="progress__filled"></div>
   </div>
-  <button class="player__button toggle" title="Toggle Play">►</button>
+  <button class="player__button toggle5" title="Toggle Play">►</button>
   <input type="range" name="volume" class="player__slider" min="0" max="1" step="0.05" value="1">
   <input type="range" name="playbackRate" class="player__slider" min="0.5" max="2" step="0.1" value="1">
   <button data-skip="-10" class="player__button">« 10s</button>
@@ -20,5 +20,31 @@ var videoPlayerMarkup = `
 videoPlayerContainer.innerHTML = videoPlayerMarkup;
 
 const player = document.querySelector('.player');
-const video = 
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle5 = player.querySelector('.toggle5');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+function togglePlay(){
+    var method = video.paused ? 'play' : 'pause';
+    video[method]();
+    // console.log('run')
+}
+
+function toggleButton(){
+    var icon = this.paused ? '►' : '❚ ❚';
+    toggle5.textContent = icon;
+
+    console.log('toggle')
+
+}
+video.addEventListener('click', togglePlay);
+toggle5.addEventListener('click', togglePlay);
+video.addEventListener('play', toggleButton);
+video.addEventListener('pause', toggleButton);
+
+
 
